dce95e6cbbb084d1e0a78556889c2a439b0fa16a
Loki.Luo <Loki.Luo@gsst.com>
2017-05-12 19:07:12 +0800

    Signed-off-by: Loki.Luo <Loki.Luo@gsst.com>

---
+++ dao/addData.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Loki.Luo on 2017/5/11.
+ */
+var mysql = require('mysql');
+var $conf = require('../conf/database');
+var $util = require('../util/util');
+var $sql = {
+    insert:'INSERT INTO user(id, title, body, published) VALUES(0,?,?,?)',
+    update:'update user set name=?, age=? where id=?',
+    delete: 'delete from user where id=?',
+    queryById: 'select * from user where id=?',
+    queryAll: 'select * from user'
+};
+
+// 使用连接池，提升性能
+var pool  = mysql.createPool($util.extend({}, $conf.connection));
+
+// 向前台返回JSON方法的简单封装
+var jsonWrite = function (res, ret) {
+    if(typeof ret === 'undefined') {
+        res.json({
+            code:'1',
+            msg: '操作失败'
+        });
+    } else {
+        res.json(ret);
+    }
+};
+
+module.exports = {
+    add: function (req, res, next) {
+        pool.getConnection(function(err, connection) {
+            // 获取前台页面传过来的参数
+            var param = req.query || req.params;
+
+            // 建立连接，向表中插入值
+            // 'INSERT INTO user(id, name, age) VALUES(0,?,?)',
+            connection.query($sql.insert, [param.title, param.body, param.published], function(err, result) {
+                if(result) {
+                    result = {
+                        code: 200,
+                        msg:'增加成功'
+                    };
+                }
+
+                // 以json形式，把操作结果返回给前台页面
+                jsonWrite(res, result);
+
+                // 释放连接
+                connection.release();
+            });
+        });
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -11,14 +11,21 @@ var userDao = require('../dao/userDao');
 var cb1 = function (req, res, next) {
     console.log('CB1');
     next();
-}
+};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource rrr');
+    res.render('addBlog');
 });
 
 router.get('/addUser', function(req, res, next) {
     userDao.add(req, res, next);
+});
+
+router.post('/addBlog', function(req, res, next) {
+    //res.send(res);
+    // res.render('addBlog');
+    console.log(res);
+    // userDao.add(req, res, next);
 });
 
 module.exports = router;
